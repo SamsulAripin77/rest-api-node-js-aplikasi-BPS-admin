37e90692e0c8180312a5bc35464c12c91823201a
arifins192@gmail.com <samsul>
2019-06-20 02:14:43 +0700

    api masuk : else if ditukar

--- auth/masuk.js
+++ auth/masuk.js
@@ -14,7 +14,7 @@ app.get('/login', (req, res) => {
         const uid = userRecord.uid
         const cekKodeWilayah = admin.database().ref('/').orderByKey().equalTo(kodeWilayah).once('value', (snapshot) => {
             if (snapshot.exists()) {
-                snapshot.forEach((data)=>{
+                snapshot.forEach((data) => {
                     console.log('berhasil login')
                     res.json({uuid: uid, wilayah: data.key})
                 })
@@ -39,48 +39,45 @@ app.get('/listadmin', (req, res) => {
     const kode_wilayah = req.body.kode_wilayah || '3202'
     const email = req.body.email || 'adminSipp@email.com'
 
-    admin.auth().getUserByEmail(email)
-        .then((userRecord) => {
-            const uid = userRecord.uid
-            
-             console.log('uid yang didapatkan adalah : ', uid)
-             listadmin()
+    admin.auth().getUserByEmail(email).then((userRecord) => {
+        const uid = userRecord.uid
 
-        })
-        .catch((error) => {
-            console.log('error terjadi di mana-mana: ', error)
-            res.status(404)
-            res.send('eror')
-        })
+        console.log('uid yang didapatkan adalah : ', uid)
+        listadmin()
+
+    }).catch((error) => {
+        console.log('error terjadi di mana-mana: ', error)
+        res.status(404)
+        res.send('eror')
+    })
 
     function listadmin() {
         const db = admin.database()
         const allValue = []
         const ref = db.ref('/admin')
         ref.once('value', (snapshot) => {
-                data = snapshot.val()
-                    // console.log(data)
-                console.log('======================================================================================')
-                snapshot.forEach((childSnapshot) => {
-                    var kel = childSnapshot.key
-                    var childData = childSnapshot.val()
-                    allValue.push(childData)
-                })
-                console.log(allValue)
-                res.send(allValue)
-                ref.off('value')
+            data = snapshot.val()
+            // console.log(data)
+            console.log('======================================================================================')
+            snapshot.forEach((childSnapshot) => {
+                var kel = childSnapshot.key
+                var childData = childSnapshot.val()
+                allValue.push(childData)
             })
-            .catch((error) => {
-                console.log('error terjadi di :', error)
-                res.status(404)
-                res.json({error : "gagal mendapatkan data admin"})
-            })
+            console.log(allValue)
+            res.send(allValue)
+            ref.off('value')
+        }).catch((error) => {
+            console.log('error terjadi di :', error)
+            res.status(404)
+            res.json({error: "gagal mendapatkan data admin"})
+        })
 
     }
 
 })
 
-//==========================================================================================
+// ==========================================================================================
 app.post("/postHero", (req, res) => {
 
     const email = req.body.email || '4234324242r5453@email.com'
@@ -100,68 +97,56 @@ app.post("/postHero", (req, res) => {
         uid: "error",
         uidAtasan: "error"
     }
- // ====================================================
-                    console.log('kode wilayah ada')
-                    const cekAtasan = admin.database()
-                    .ref('/admin/')
-                    .orderByChild("username")
-                    .equalTo(username).once('value', function (snapshot) {
-                        if (snapshot.exists()) {
-                            console.lo(username)
-                            console.log(snapshot.val())
-                            console.log('------------------------------------------------------------------------------' + '-');
-                         // referensi = https://github.com/firebase/functions-samples/issues/265 go
-                            snapshot.forEach(function (childSnapshot) {
-                                var key = childSnapshot.key;
-                                var childData = childSnapshot.val();
-                                // this will be the actual email value found
-                                const uidAtasan = childData.uid
-                                const nama_atasan = childData.nama
-                                console.log(childData.uid);
-                                console.log(childData.nama);
-                                // res.send('berhasil mendapat uid')
-                                // =====================================================
-                                if (kodeWilayah === undefined) {
-                                    res.send("isi kode wilayah")
-                                    console.log("kode wilayah kosong");
-                                } else if (email === undefined) {
-                                    res.send("isi email")
-                                    console.log("email kosong");
-                                } else { // ===================================================== create user
-                                    admin.auth().createUser({
-                                        email: email,
-                                        password: password,
-                                    }).then(function (userRecord) { // See the UserRecord reference doc for the contents of userRecord.
-                                        console.log("Successfully created new user:", userRecord.uid);
-                                        uid = userRecord.uid
-                                        const db = admin.database()
-                                        const ref = db.ref(`/admin/${uid}`)
-                                        ref.set({
-                                            kodeWilayah,
-                                            username,
-                                            password,
-                                            uid
-                                        })
-                                    }).catch(function (error) {
-                                        res.status(405)
-                                        res.json(errorJson)
-                                        console.log("Error creating new user:", error);
-                                    });
-                                }
-                            })
-
-                        } else {
-                            console.log('username atasan tidak ditemukan')
-                            res.status(444)
-                            res.json(errorJson)
-                        }
-                    }).catch((error) => {
-                        console.log.json("eror di pencarian uid atasan l:", error)
-                        res.status(407)
+    // ====================================================
+    console.log('kode wilayah ada')
+    const cekAtasan = admin.database().ref('/admin/').orderByChild("username").equalTo(username).once('value', function (snapshot) {
+        if (snapshot.exists()) {
+            console.log('username yang sama sudah ada')
+            res.status(444)
+            res.json(errorJson)
+        } else {
+            console.lo(username)
+            console.log(snapshot.val())
+            console.log('------------------------------------------------------------------------------' + '-');
+            // referensi = https://github.com/firebase/functions-samples/issues/265 go
+            snapshot.forEach(function (childSnapshot) {
+                var key = childSnapshot.key;
+                var childData = childSnapshot.val();
+                // this will be the actual email value found
+                const uidAtasan = childData.uid
+                const nama_atasan = childData.nama
+                console.log(childData.uid);
+                console.log(childData.nama);
+                // res.send('berhasil mendapat uid')
+                // =====================================================
+                if (kodeWilayah === undefined) {
+                    res.send("isi kode wilayah")
+                    console.log("kode wilayah kosong");
+                } else if (email === undefined) {
+                    res.send("isi email")
+                    console.log("email kosong");
+                } else { // ===================================================== create user
+                    admin.auth().createUser({email: email, password: password}).then(function (userRecord) { // See the UserRecord reference doc for the contents of userRecord.
+                        console.log("Successfully created new user:", userRecord.uid);
+                        uid = userRecord.uid
+                        const db = admin.database()
+                        const ref = db.ref(`/admin/${uid}`)
+                        ref.set({kodeWilayah, username, password, uid})
+                    }).catch(function (error) {
+                        res.status(405)
                         res.json(errorJson)
-                    })
-                  
+                        console.log("Error creating new user:", error);
+                    });
+                }
+            })
+        }
+    }).catch((error) => {
+        console.log.json("eror di pencarian uid atasan l:", error)
+        res.status(407)
+        res.json(errorJson)
+    })
+
 })
 
-//==========================================================================================
+// ==========================================================================================
 module.exports = app
